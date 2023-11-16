dcd4fa2609c3ec2abcb7e64157f15dca7d61b2ad
diegoperez18 <diegoperezfernandez47@gmail.com>
2023-11-16 18:07:08 +0000

    goingWell

--- static/js/script.js
+++ static/js/script.js
@@ -7,4 +7,10 @@ function displaySugar(myimage) {
 	myimage.src = "../../static/img/sugar.png";
 }
 
+function displayBreadtwo(myimage) {
+	        myimage.src="../../static/img/bread2.png";
+}
 
+function displayBread(myimage) {
+	        myimage.src = "../../static/img/bread.png";
+}
